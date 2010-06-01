24a92ac9155f219552a153e68b52ab7faae71a3d
Alan Gutierrez <alan@blogometer.com>
2010-06-01 17:16:32 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/manifold/mix/ManifoldProject.java
+++ src/mix/java/com/goodworkalan/manifold/mix/ManifoldProject.java
@@ -1,21 +1,28 @@
 package com.goodworkalan.manifold.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class ManifoldProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Manifold.
+ *
+ * @author Alan Gutierrez
+ */
+public class ManifoldProject implements ProjectModule {
+    /**
+     * Build the project definition for Manifold.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan", "manifold", "0.1"))
-                .test()
-                    .depends()
-                        .artifact(new Artifact("org.testng/testng/5.10"))
-                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
-                        .end()
+                .produces("com.goodworkalan/manifold/0.1")
+                .depends()
+                    .development("org.testng/testng-jdk15/5.10")
+                    .development("org.mockito/mockito-core/1.6")
                     .end()
                 .end()
             .end();
